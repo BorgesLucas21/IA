bb7b0f28705e9e366a18da0540d72be93380c193
BorgesLucas21 <141366250+BorgesLucas21@users.noreply.github.com>
2025-05-23 11:10:40 -0300

    9

--- script.js
+++ script.js
@@ -5,6 +5,11 @@ const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 const perguntas = [
     {
-        enunciado
-    }
-]
+        enunciado: "pergunta1",
+        alternativas: ["alternativa1,alternativa2"]
+    },
+    {
+        enunciado: "pergunta2",
+        alternativas: ["alternativa1,alternativa2"]
+    },
+];
